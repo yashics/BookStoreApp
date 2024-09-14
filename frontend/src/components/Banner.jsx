355fd5ee140f2c7ec77414aeb422f415b9745206
yashics <yashi230602@gmail.com>
2024-09-14 10:03:51 +0530

    last commit

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -27,7 +27,7 @@ function Banner  ()  {
   <input type="text" className="grow" placeholder="Email" />
 </label>
        </div>
-       <button className="btn btn-secondary mt-8">Get Started </button>
+       <button className="btn btn-secondary mt-8">Get Started  </button>
       
         </div>
         <div className='order-1 w-full md:w-1/2 px-12 pt-10'>
